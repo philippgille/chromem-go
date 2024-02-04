11a4168f8a24779fffefde8f15bd02db1df85f2e
Philipp Gillé <philipp.gille@gmail.com>
2024-02-04 15:37:23 +0100

    Rename Client to DB

--- client.go
+++ client.go
@@ -10,19 +10,19 @@ import (
 // own function, using any model you like.
 type EmbeddingFunc func(ctx context.Context, document string) ([]float32, error)
 
-// Client is the chromem-go client. It holds collections, which hold documents.
+// DB is the chromem-go database. It holds collections, which hold documents.
 //
-//	+--------+    1-n    +------------+    n-n    +----------+
-//	| Client |-----------| Collection |-----------| Document |
-//	+--------+           +------------+           +----------+
-type Client struct {
+//	+----+    1-n    +------------+    n-n    +----------+
+//	| DB |-----------| Collection |-----------| Document |
+//	+----+           +------------+           +----------+
+type DB struct {
 	collections     map[string]*Collection
 	collectionsLock sync.RWMutex
 }
 
-// NewClient creates a new chromem-go client.
-func NewClient() *Client {
-	return &Client{
+// NewDB creates a new chromem-go DB.
+func NewDB() *DB {
+	return &DB{
 		collections: make(map[string]*Collection),
 	}
 }
@@ -33,7 +33,7 @@ func NewClient() *Client {
 //   - metadata: Optional metadata to associate with the collection.
 //   - embeddingFunc: Optional function to use to embed documents.
 //     Uses the default embedding function if not provided.
-func (c *Client) CreateCollection(name string, metadata map[string]string, embeddingFunc EmbeddingFunc) *Collection {
+func (c *DB) CreateCollection(name string, metadata map[string]string, embeddingFunc EmbeddingFunc) *Collection {
 	if embeddingFunc == nil {
 		embeddingFunc = CreateEmbeddingsDefault()
 	}
