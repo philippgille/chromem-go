08fc38a17313e3415045b86705875bfe7feeea46
Philipp Gillé <philipp.gille@gmail.com>
2024-02-17 20:19:57 +0100

    Add DB.GetCollection()

--- db_test.go
+++ db_test.go
@@ -47,3 +47,34 @@ func TestDB_ListCollections(t *testing.T) {
 		t.Error("expected 1 collection, got", len(db.ListCollections()))
 	}
 }
+
+func TestDB_GetCollection(t *testing.T) {
+	// Values in the collection
+	name := "test"
+	metadata := map[string]string{"foo": "bar"}
+	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
+		return []float32{-0.1, 0.1, 0.2}, nil
+	}
+
+	// Create initial collection
+	db := chromem.NewDB()
+	// We ignore the return value. CreateCollection is tested elsewhere.
+	_ = db.CreateCollection(name, metadata, embeddingFunc)
+
+	// Get collection
+	c := db.GetCollection(name)
+
+	// Check expectations
+	if c.Name != name {
+		t.Error("expected name", name, "got", c.Name)
+	}
+	if len(c.Metadata) != 1 {
+		t.Error("expected 1 metadata, got", len(c.Metadata))
+	}
+	if c.Metadata["foo"] != "bar" {
+		t.Error("expected metadata", metadata, "got", c.Metadata)
+	}
+	// TODO: Check documents content as soon as we have access to them
+	// TODO: Same for the EmbeddingFunc
+	// TODO: Check documents map being a copy as soon as we have access to it
+}
