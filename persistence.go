91439122aec943bd527198a4790405675d326828
Philipp Gillé <philipp.gille@gmail.com>
2024-02-25 00:53:19 +0100

    Shorten generated file names to 8 chars

--- persistence.go
+++ persistence.go
@@ -10,7 +10,10 @@ import (
 
 func hash2hex(name string) string {
 	hash := sha256.Sum256([]byte(name))
-	return hex.EncodeToString(hash[:])
+	// We encode 4 of the 32 bytes (32 out of 256 bits), so 8 hex characters.
+	// It's enough to avoid collisions in reasonable amounts of documents per collection
+	// and being shorter is better for file paths.
+	return hex.EncodeToString(hash[:4])
 }
 
 // persist persists an object to a file at the given path.
