cf2220d04083f624140fdb0bed1b7dccf5b66f8e
Philipp Gillé <philipp.gille@gmail.com>
2024-03-02 13:11:49 +0100

    Add Collection.Count() method

--- collection_test.go
+++ collection_test.go
@@ -34,3 +34,34 @@ func TestCollection_Add(t *testing.T) {
 
 	// TODO: Check expectations when documents become accessible
 }
+
+func TestCollection_Count(t *testing.T) {
+	// Create collection
+	db := chromem.NewDB()
+	name := "test"
+	metadata := map[string]string{"foo": "bar"}
+	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
+		return []float32{-0.1, 0.1, 0.2}, nil
+	}
+	c, err := db.CreateCollection(name, metadata, embeddingFunc)
+	if err != nil {
+		t.Error("expected no error, got", err)
+	}
+	if c == nil {
+		t.Error("expected collection, got nil")
+	}
+
+	// Add documents
+	ids := []string{"1", "2"}
+	metadatas := []map[string]string{{"foo": "bar"}, {"a": "b"}}
+	documents := []string{"hello world", "hallo welt"}
+	err = c.Add(context.Background(), ids, nil, metadatas, documents)
+	if err != nil {
+		t.Error("expected nil, got", err)
+	}
+
+	// Check count
+	if c.Count() != 2 {
+		t.Error("expected 2, got", c.Count())
+	}
+}
