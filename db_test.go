e162b623cb3b903f7835a4cc471243289033e6dd
Philipp Gillé <philipp.gille@gmail.com>
2024-03-05 20:10:09 +0100

    Extend DB tests as whitebox

--- db_test.go
+++ db_test.go
@@ -1,21 +1,21 @@
-package chromem_test
+package chromem
 
 import (
 	"context"
+	"slices"
 	"testing"
-
-	"github.com/philippgille/chromem-go"
 )
 
 func TestDB_CreateCollection(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
-	db := chromem.NewDB()
+	db := NewDB()
 
 	t.Run("OK", func(t *testing.T) {
 		c, err := db.CreateCollection(name, metadata, embeddingFunc)
@@ -23,14 +23,50 @@ func TestDB_CreateCollection(t *testing.T) {
 			t.Error("expected no error, got", err)
 		}
 		if c == nil {
-			t.Error("expected collection, got nil")
+			t.Fatal("expected collection, got nil")
 		}
 
 		// Check expectations
+
+		// DB should have one collection now
+		if len(db.collections) != 1 {
+			t.Error("expected 1 collection, got", len(db.collections))
+		}
+		// The collection should be the one we just created
+		c2, ok := db.collections[name]
+		if !ok {
+			t.Error("expected collection", name, "not found")
+		}
+		if c2.Name != name {
+			t.Error("expected name", name, "got", c2.Name)
+		}
+		// The returned collection should also be the same
 		if c.Name != name {
 			t.Error("expected name", name, "got", c.Name)
 		}
-		// TODO: Check metadata etc when they become accessible
+		// The collection's persistent dir should be empty
+		if c.persistDirectory != "" {
+			t.Error("expected empty persistent directory, got", c.persistDirectory)
+		}
+		// It's metadata should match
+		if len(c.metadata) != 1 || c.metadata["foo"] != "bar" {
+			t.Error("expected metadata", metadata, "got", c.metadata)
+		}
+		// Documents should be empty, but not nil
+		if c.documents == nil {
+			t.Error("expected non-nil documents, got nil")
+		}
+		if len(c.documents) != 0 {
+			t.Error("expected empty documents, got", len(c.documents))
+		}
+		// The embedding function should be the one we passed
+		gotVectors, err := c.embed(context.Background(), "test")
+		if err != nil {
+			t.Error("expected no error, got", err)
+		}
+		if !slices.Equal(gotVectors, vectors) {
+			t.Error("expected vectors", vectors, "got", gotVectors)
+		}
 	})
 
 	t.Run("NOK - Empty name", func(t *testing.T) {
@@ -45,12 +81,13 @@ func TestDB_ListCollections(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
 	// Create initial collection
-	db := chromem.NewDB()
+	db := NewDB()
 	// We ignore the return value. CreateCollection is tested elsewhere.
 	_, err := db.CreateCollection(name, metadata, embeddingFunc)
 	if err != nil {
@@ -61,9 +98,12 @@ func TestDB_ListCollections(t *testing.T) {
 	res := db.ListCollections()
 
 	// Check expectations
+
+	// Should've returned a map with one collection
 	if len(res) != 1 {
 		t.Error("expected 1 collection, got", len(res))
 	}
+	// The collection should be the one we just created
 	c, ok := res[name]
 	if !ok {
 		t.Error("expected collection", name, "not found")
@@ -71,18 +111,33 @@ func TestDB_ListCollections(t *testing.T) {
 	if c.Name != name {
 		t.Error("expected name", name, "got", c.Name)
 	}
-	// TODO: Check metadata when it's accessible (e.g. with GetMetadata())
-	// if len(c.Metadata) != 1 {
-	// 	t.Error("expected 1 metadata, got", len(c.Metadata))
-	// }
-	// if c.Metadata["foo"] != "bar" {
-	// 	t.Error("expected metadata", metadata, "got", c.Metadata)
-	// }
-	// TODO: Same for documents and EmbeddingFunc
+	// The collection's persistent dir should be empty
+	if c.persistDirectory != "" {
+		t.Error("expected empty persistent directory, got", c.persistDirectory)
+	}
+	// It's metadata should match
+	if len(c.metadata) != 1 || c.metadata["foo"] != "bar" {
+		t.Error("expected metadata", metadata, "got", c.metadata)
+	}
+	// Documents should be empty, but not nil
+	if c.documents == nil {
+		t.Error("expected non-nil documents, got nil")
+	}
+	if len(c.documents) != 0 {
+		t.Error("expected empty documents, got", len(c.documents))
+	}
+	// The embedding function should be the one we passed
+	gotVectors, err := c.embed(context.Background(), "test")
+	if err != nil {
+		t.Error("expected no error, got", err)
+	}
+	if !slices.Equal(gotVectors, vectors) {
+		t.Error("expected vectors", vectors, "got", gotVectors)
+	}
 
 	// And it should be a copy. Adding a value here should not reflect on the DB's
 	// collection.
-	res["foo"] = &chromem.Collection{}
+	res["foo"] = &Collection{}
 	if len(db.ListCollections()) != 1 {
 		t.Error("expected 1 collection, got", len(db.ListCollections()))
 	}
@@ -92,12 +147,13 @@ func TestDB_GetCollection(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
 	// Create initial collection
-	db := chromem.NewDB()
+	db := NewDB()
 	// We ignore the return value. CreateCollection is tested elsewhere.
 	_, err := db.CreateCollection(name, metadata, embeddingFunc)
 	if err != nil {
@@ -111,29 +167,43 @@ func TestDB_GetCollection(t *testing.T) {
 	if c.Name != name {
 		t.Error("expected name", name, "got", c.Name)
 	}
-	// TODO: Check metadata when it's accessible (e.g. with GetMetadata())
-	// if len(c.Metadata) != 1 {
-	// 	t.Error("expected 1 metadata, got", len(c.Metadata))
-	// }
-	// if c.Metadata["foo"] != "bar" {
-	// 	t.Error("expected metadata", metadata, "got", c.Metadata)
-	// }
-	// TODO: Check documents content as soon as we have access to them
-	// TODO: Same for the EmbeddingFunc
-	// TODO: Check documents map being a copy as soon as we have access to it
+	// The collection's persistent dir should be empty
+	if c.persistDirectory != "" {
+		t.Error("expected empty persistent directory, got", c.persistDirectory)
+	}
+	// It's metadata should match
+	if len(c.metadata) != 1 || c.metadata["foo"] != "bar" {
+		t.Error("expected metadata", metadata, "got", c.metadata)
+	}
+	// Documents should be empty, but not nil
+	if c.documents == nil {
+		t.Error("expected non-nil documents, got nil")
+	}
+	if len(c.documents) != 0 {
+		t.Error("expected empty documents, got", len(c.documents))
+	}
+	// The embedding function should be the one we passed
+	gotVectors, err := c.embed(context.Background(), "test")
+	if err != nil {
+		t.Error("expected no error, got", err)
+	}
+	if !slices.Equal(gotVectors, vectors) {
+		t.Error("expected vectors", vectors, "got", gotVectors)
+	}
 }
 
 func TestDB_GetOrCreateCollection(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
 	t.Run("Get", func(t *testing.T) {
 		// Create initial collection
-		db := chromem.NewDB()
+		db := NewDB()
 		// Create collection so that the GetOrCreateCollection() call below only
 		// gets it.
 		// We ignore the return value. CreateCollection is tested elsewhere.
@@ -150,19 +220,41 @@ func TestDB_GetOrCreateCollection(t *testing.T) {
 			t.Error("expected no error, got", err)
 		}
 		if c == nil {
-			t.Error("expected collection, got nil")
+			t.Fatal("expected collection, got nil")
 		}
 
 		// Check expectations
 		if c.Name != name {
 			t.Error("expected name", name, "got", c.Name)
 		}
-		// TODO: Check metadata when it's accessible (e.g. with GetMetadata())
+		// The collection's persistent dir should be empty
+		if c.persistDirectory != "" {
+			t.Error("expected empty persistent directory, got", c.persistDirectory)
+		}
+		// It's metadata should match
+		if len(c.metadata) != 1 || c.metadata["foo"] != "bar" {
+			t.Error("expected metadata", metadata, "got", c.metadata)
+		}
+		// Documents should be empty, but not nil
+		if c.documents == nil {
+			t.Error("expected non-nil documents, got nil")
+		}
+		if len(c.documents) != 0 {
+			t.Error("expected empty documents, got", len(c.documents))
+		}
+		// The embedding function should be the one we passed
+		gotVectors, err := c.embed(context.Background(), "test")
+		if err != nil {
+			t.Error("expected no error, got", err)
+		}
+		if !slices.Equal(gotVectors, vectors) {
+			t.Error("expected vectors", vectors, "got", gotVectors)
+		}
 	})
 
 	t.Run("Create", func(t *testing.T) {
 		// Create initial collection
-		db := chromem.NewDB()
+		db := NewDB()
 
 		// Call GetOrCreateCollection()
 		c, err := db.GetOrCreateCollection(name, metadata, embeddingFunc)
@@ -170,14 +262,36 @@ func TestDB_GetOrCreateCollection(t *testing.T) {
 			t.Error("expected no error, got", err)
 		}
 		if c == nil {
-			t.Error("expected collection, got nil")
+			t.Fatal("expected collection, got nil")
 		}
 
 		// Check like we check CreateCollection()
 		if c.Name != name {
 			t.Error("expected name", name, "got", c.Name)
 		}
-		// TODO: Check metadata when it's accessible (e.g. with GetMetadata())
+		// The collection's persistent dir should be empty
+		if c.persistDirectory != "" {
+			t.Error("expected empty persistent directory, got", c.persistDirectory)
+		}
+		// It's metadata should match
+		if len(c.metadata) != 1 || c.metadata["foo"] != "bar" {
+			t.Error("expected metadata", metadata, "got", c.metadata)
+		}
+		// Documents should be empty, but not nil
+		if c.documents == nil {
+			t.Error("expected non-nil documents, got nil")
+		}
+		if len(c.documents) != 0 {
+			t.Error("expected empty documents, got", len(c.documents))
+		}
+		// The embedding function should be the one we passed
+		gotVectors, err := c.embed(context.Background(), "test")
+		if err != nil {
+			t.Error("expected no error, got", err)
+		}
+		if !slices.Equal(gotVectors, vectors) {
+			t.Error("expected vectors", vectors, "got", gotVectors)
+		}
 	})
 }
 
@@ -185,12 +299,13 @@ func TestDB_DeleteCollection(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
 	// Create initial collection
-	db := chromem.NewDB()
+	db := NewDB()
 	// We ignore the return value. CreateCollection is tested elsewhere.
 	_, err := db.CreateCollection(name, metadata, embeddingFunc)
 	if err != nil {
@@ -206,18 +321,23 @@ func TestDB_DeleteCollection(t *testing.T) {
 	if len(db.ListCollections()) != 0 {
 		t.Error("expected 0 collections, got", len(db.ListCollections()))
 	}
+	// Also check internally
+	if len(db.collections) != 0 {
+		t.Error("expected 0 collections, got", len(db.collections))
+	}
 }
 
 func TestDB_Reset(t *testing.T) {
 	// Values in the collection
 	name := "test"
 	metadata := map[string]string{"foo": "bar"}
+	vectors := []float32{-0.1, 0.1, 0.2}
 	embeddingFunc := func(_ context.Context, _ string) ([]float32, error) {
-		return []float32{-0.1, 0.1, 0.2}, nil
+		return vectors, nil
 	}
 
 	// Create initial collection
-	db := chromem.NewDB()
+	db := NewDB()
 	// We ignore the return value. CreateCollection is tested elsewhere.
 	_, err := db.CreateCollection(name, metadata, embeddingFunc)
 	if err != nil {
@@ -232,5 +352,9 @@ func TestDB_Reset(t *testing.T) {
 	// because it's tested elsewhere.
 	if len(db.ListCollections()) != 0 {
 		t.Error("expected 0 collections, got", len(db.ListCollections()))
+	}
+	// Also check internally
+	if len(db.collections) != 0 {
+		t.Error("expected 0 collections, got", len(db.collections))
 	}
 }
