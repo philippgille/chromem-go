ddc3f8ada11676685a22aa0172a6c0897f3a37ae
Philipp Gillé <philipp.gille@gmail.com>
2024-04-25 23:04:40 +0200

    Revert "Merge pull request #68 from philippgille/stop-exporting-openai-baseurl"
    This reverts commit 04d0fd54985187325f215a65500cc4cb7d27dd90, reversing
    changes made to 6886041cdf237523e7960e75198006c159ab3c88.

--- embed_openai.go
+++ embed_openai.go
@@ -12,7 +12,7 @@ import (
 	"sync"
 )
 
-const baseURLOpenAI = "https://api.openai.com/v1"
+const BaseURLOpenAI = "https://api.openai.com/v1"
 
 type EmbeddingModelOpenAI string
 
@@ -42,7 +42,7 @@ func NewEmbeddingFuncDefault() EmbeddingFunc {
 func NewEmbeddingFuncOpenAI(apiKey string, model EmbeddingModelOpenAI) EmbeddingFunc {
 	// OpenAI embeddings are normalized
 	normalized := true
-	return NewEmbeddingFuncOpenAICompat(baseURLOpenAI, apiKey, string(model), &normalized)
+	return NewEmbeddingFuncOpenAICompat(BaseURLOpenAI, apiKey, string(model), &normalized)
 }
 
 // NewEmbeddingFuncOpenAICompat returns a function that creates embeddings for a text
